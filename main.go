282f05b371c745cfa5061540c657174b567e82ad
Brian Tomlinson <brian@smartm.no>
2015-01-31 21:02:27 +0100

    clean up a bit, let cleanExit do the exiting

--- main.go
+++ main.go
@@ -10,19 +10,21 @@ import (
 
 const DELAY = 25
 
-func cleanExit() {
-	fmt.Printf("Exiting...\n")
+func cleanExit(sigChan chan<- os.Signal) {
+	fmt.Printf("Exiting with signal: %v\n", sigChan)
+	os.Exit(1)
 }
 
 func main() {
 	sigChan := make(chan os.Signal, 1)
+
 	signal.Notify(sigChan, os.Interrupt)
 	signal.Notify(sigChan, syscall.SIGTERM)
 	signal.Notify(sigChan, syscall.SIGKILL)
+
 	go func() {
 		<-sigChan
-		cleanExit()
-		os.Exit(1)
+		cleanExit(sigChan)
 	}()
 
 	for {
